587af0785c6dece06d1257b898c9f84402527868
Simon Maurer <maurer.simon@live.de>
2022-08-23 20:01:37 +0200

    mod and value buttons work properly now

--- sandbox/version3/charactersheet.js
+++ sandbox/version3/charactersheet.js
@@ -2,31 +2,50 @@ console.log("alive");
 
 /*** BEGIN ATTRIBUTE BUTTONS***/
 
-const buttons = document.querySelectorAll(".wrapper button");
-buttons.forEach(element => element.addEventListener("click", handleClick));
+const unsignedButtons = document.querySelectorAll(".wrapper button.unsigned");
+const signedButtons = document.querySelectorAll(".wrapper button.signed");
 
-function handleClick(event) {
+unsignedButtons.forEach(button => button.addEventListener("click", handleUnsignedClick));
+signedButtons.forEach(button => button.addEventListener("click", handleSignedClick));
+
+function handleUnsignedClick(event) {
+    const [element, target] = elementTarget(event);
+    handle(element, target, false);
+    updateModifier(target.textContent, element.parentElement.previousElementSibling.previousElementSibling);
+}
+
+function handleSignedClick(event) {
+    const [element, target] = elementTarget(event);
+    handle(element, target, true);
+    updateModifier(Number(target.textContent) + 10, target);
+}
+
+function handle(element, target, signed) {
+    element.classList.contains('increment') ? increment(target, signed) : decrement(target, signed);
+}
+
+function elementTarget(event) {
     const element = event.target;
-    const target = element.parentElement.querySelector(".real.value");
-    if (element.className === "increment") increment(target); else decrement(target);
-    updateModifier(target, element.parentElement.previousElementSibling.previousElementSibling);
+    return [element, element.parentElement.querySelector(".change.value")];
 }
 
-function change(value, target) {
-    value ? target.textContent++ : target.textContent--; // how tf does this work, it's amazing
+function change(inc, target) {
+    inc ? target.textContent++ : target.textContent--; // how tf does this work, it's amazing
 }
 
-function increment(target) {
-    if (target.textContent < 20) change(true, target);
+function increment(target, signed) {
+    if ((signed && target.textContent < 10) || (!signed && target.textContent < 20)) change(true, target);
 }
 
-function decrement(target) {
-    if (target.textContent > 0) change(false, target)
+function decrement(target, signed) {
+    if ((signed && target.textContent > -10) || (!signed && target.textContent > 0)) change(false, target);
 }
 
-function updateModifier(real, mod) {
-    const val = real.textContent - 10;
-    mod.textContent = val > 0 ? "+" + val : val;
+function updateModifier(value, target) {
+    console.log(value)
+    console.log(target)
+    const val = value - 10;
+    target.textContent = val > 0 ? "+" + val : val;
 }
 
 /*** END ATTRIBUTE BUTTONS ***/
